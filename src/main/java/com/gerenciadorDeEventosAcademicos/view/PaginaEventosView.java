affafcedcb61e6a388b194214405a10cd0298558
joaoalbertorsc <jaoalbertorsc@gmail.com>
2024-08-31 10:37:47 -0300

    commit config

---
+++ src/main/java/com/gerenciadorDeEventosAcademicos/view/PaginaEventosView.java
@@ -0,0 +1,102 @@
+package com.gerenciadorDeEventosAcademicos.view;
+import com.gerenciadorDeEventosAcademicos.controller.PaginaEventosController;
+import com.gerenciadorDeEventosAcademicos.model.*;
+import java.util.Scanner;
+
+public class PaginaEventosView implements Observer{
+
+    private Model model;
+    private Evento evento;
+    private PaginaEventosController controller;
+    private int numeroEventoEscolhido;
+    private int escolhaUsuario;
+    public void initPaginaEventosView(Model model) {  // iniciando a pagina de eventos
+        this.model = model;
+        controller = new PaginaEventosController();
+        controller.initPaginaEventosController(model, this);
+        model.attachObserver(this);
+        paginaEventos();
+    }
+
+    private void paginaEventos() { // view da pagina de eventos
+        Scanner scanner = new Scanner(System.in);
+        String[] opcoes = {"[1] - Escolher evento", "[2] - Voltar para a pagina inicial","index 2"};
+        System.out.println("======================");
+        System.out.println("PAGINA DE EVENTOS");
+        System.out.println("======================");
+        System.out.println();
+        controller.eventosDisponiveis();
+        if (model.getUsuario() instanceof Organizador){
+            opcoes[1] = "[2] - Criar evento";
+            opcoes[2] = "[3] - Voltar para a pagina inicial";
+            System.out.println(opcoes[0]);
+            System.out.println(opcoes[1]);
+            System.out.println(opcoes[2]);
+        } else {
+            System.out.println(opcoes[0]);
+            System.out.println(opcoes[1]);
+        }
+        System.out.println("Escolha uma opcao: ");
+        escolhaUsuario = scanner.nextInt();
+        scanner.nextLine();
+        controller.handleEvent(escolhaUsuario);
+        numeroEventoEscolhido = scanner.nextInt();
+        model.detachObserver(this);
+    }
+    public static String msgNenhumEventoCadastrado = "Nenhum evento cadastrado...";
+    public static String msgNumeroEventoCadastrado = "Digite o numero do evento desejado: ";
+    public static String msgIdInvalido = "ID invalido.\nConfira o numero e tente novamente.";
+    public static String totalEventosDisponiveis(Model model){
+        return "Total eventos disponiveis: " + model.getEventosCadastrados().size();
+    }
+    public static String msgListaEventos = "Lista de eventos:";
+    public void exibirMensagem(String mensagem){
+        System.out.println();
+        System.out.println(mensagem);
+        System.out.println();
+    }
+    public int getNumeroEventoEscolhido() {
+        return numeroEventoEscolhido;
+    }
+
+    public void setNumeroEventoEscolhido(int numeroEventoEscolhido) {
+        this.numeroEventoEscolhido = numeroEventoEscolhido;
+    }
+
+    public int getEscolhaUsuario() {
+        return escolhaUsuario;
+    }
+
+    public void setEscolhaUsuario(int escolhaUsuario) {
+        this.escolhaUsuario = escolhaUsuario;
+    }
+
+    public Model getModel() {
+        return model;
+    }
+
+    public void setModel(Model model) {
+        this.model = model;
+    }
+
+    public Evento getEvento() {
+        return evento;
+    }
+
+    public void setEvento(Evento evento) {
+        this.evento = evento;
+    }
+
+    public PaginaEventosController getController() {
+        return controller;
+    }
+
+    public void setController(PaginaEventosController controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
